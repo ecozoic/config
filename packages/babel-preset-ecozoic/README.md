a34002cc0f6936f40d76b08bd796c0bf60043a44
ecozoic <johnnn.mitchell@gmail.com>
2018-06-14 10:40:10 -0400

    add babel-preset-ecozoic

---
+++ packages/babel-preset-ecozoic/README.md
@@ -0,0 +1,31 @@
+# babel-preset-ecozoic
+
+A Babel preset to quickly bootstrap a new application
+
+## Features
+### Babel Presets
+* `babel-preset-env` (`babel-preset-es2015`, `babel-preset-es2016`, `babel-preset-es2017`)
+* `babel-preset-react`
+* `babel-preset-stage-0`, `babel-preset-stage-1`, `babel-preset-stage-2`, `babel-preset-stage-3`
+
+### Babel Plugins
+* React Hot Loader
+* Lodash
+* Styled Components
+
+### Experimental Features
+* Decorators (Legacy)
+
+## Installation
+```bash
+npm install @ecozoic/babel-preset-ecozoic
+```
+
+`.babelrc`
+```json
+{
+  "presets": ["@ecozoic/ecozoic"]
+}
+```
+
+## Options
---
+++ packages/babel-preset-ecozoic/src/index.js
@@ -0,0 +1,61 @@
+import presetEnv from 'babel-preset-env';
+import presetReact from 'babel-preset-react';
+
+import syntaxDynamicImport from 'babel-plugin-syntax-dynamic-import';
+import transformAsyncGeneratorFunctions from 'babel-plugin-transform-async-generator-functions';
+import transformClassProperties from 'babel-plugin-transform-class-properties';
+import transformDecoratorsLegacy from 'babel-plugin-transform-decorators-legacy';
+import transformDoExpressions from 'babel-plugin-transform-do-expressions';
+import transformES2015ModulesCommonJS from 'babel-plugin-transform-es2015-modules-commonjs';
+import transformExportExtensions from 'babel-plugin-transform-export-extensions';
+import transformFunctionBind from 'babel-plugin-transform-function-bind';
+import transformObjectRestSpread from 'babel-plugin-transform-object-rest-spread';
+
+import reactHotLoader from 'react-hot-loader/babel';
+import lodash from 'babel-plugin-lodash';
+import styledComponents from 'babel-plugin-styled-components';
+
+// plugins run before presets
+// plugin order: first to last
+// preset order: last to first
+
+export default function buildPreset(context, opts = {}) {
+  const isTest = process.env.NODE_ENV === 'test';
+
+  return {
+    presets: [
+      opts.env !== false && [presetEnv, opts.env], // set modules: false by default
+      opts.react !== false && presetReact,
+    ].filter(Boolean),
+    plugins: [
+      // styled-components
+      opts.styled !== false && [styledComponents, opts.styled],
+
+      // decorators
+      opts.decorators !== false && transformDecoratorsLegacy,
+
+      // hmr
+      opts.hmr !== false && reactHotLoader,
+
+      // lodash
+      opts.lodash !== false && [lodash, opts.lodash],
+
+      // stage 0
+      transformDoExpressions,
+      transformFunctionBind,
+
+      // stage 1
+      transformExportExtensions,
+
+      // stage 2
+      syntaxDynamicImport,
+      transformClassProperties, // spec: false
+      
+      // stage 3
+      transformAsyncGeneratorFunctions,
+      transformObjectRestSpread, // useBuiltIns: false
+
+      isTest && transformES2015ModulesCommonJS, // loose: false, strict: false, noInterop: false, allowTopLevelThis: false
+    ].filter(Boolean),
+  };
+}
